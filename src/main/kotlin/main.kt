12e3e2dbbf7b9d7bd069043b9b9f33742d04a46b
joamag <joamag@gmail.com>
2017-10-15 18:21:17 +0100

    initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,17 @@
+import org.jetbrains.ktor.host.embeddedServer
+import org.jetbrains.ktor.http.ContentType
+import org.jetbrains.ktor.netty.Netty
+import org.jetbrains.ktor.response.respondText
+import org.jetbrains.ktor.routing.get
+import org.jetbrains.ktor.routing.routing
+
+fun main(args: Array<String>) {
+    val server = embeddedServer(Netty, 8080) {
+        routing {
+            get("/") {
+                call.respondText("Hello, world!", ContentType.Text.Html)
+            }
+        }
+    }
+    server.start(wait = true)
+}
