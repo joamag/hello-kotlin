b97216091aabd7912a3ca8f50348618902347c5e
joamag <joamag@gmail.com>
2017-11-01 13:11:30 +0000

    new code cnhanges

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -5,13 +5,30 @@ import org.jetbrains.ktor.response.respondText
 import org.jetbrains.ktor.routing.get
 import org.jetbrains.ktor.routing.routing
 
+/**
+ * The default (bind) host to be used when none is defined,
+ * notice that this values is permissive by default.
+ */
+val host = "0.0.0.0"
+
+/**
+ * The default port to be used when none is defined.
+ */
+val port = 8080
+
 fun main(args: Array<String>) {
-    val server = embeddedServer(Netty, 8080) {
+    // creates the server object with the provided port and host
+    // values as defined by standard
+    val server = embeddedServer(Netty, port = port, host = host) {
         routing {
-            get("/") {
+            get(path = "/") {
                 call.respondText("Hello, world!", ContentType.Text.Html)
             }
         }
     }
+
+    // prints a message about the initial operation of the server
+    // ans starts the blocking serving operation
+    print("Starting server on $host:$port")
     server.start(wait = true)
 }
