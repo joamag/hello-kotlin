2d6a5b9ccd5aeaa130e5da5a7118794e5e5fee7f
João Magalhães <joamag@gmail.com>
2022-12-04 12:12:31 +0000

    feat: bumped dependencies

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,9 +1,9 @@
 import ch.qos.logback.classic.Level
 import ch.qos.logback.classic.Logger
-import io.ktor.application.*
+import io.ktor.server.application.*
 import io.ktor.http.*
-import io.ktor.response.*
-import io.ktor.routing.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.netty.util.Version
